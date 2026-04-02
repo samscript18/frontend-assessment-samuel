a931249dad28f7a21e3db5afb8eb780c6b2a5b51
samscript18 <adeolasam20@gmail.com>
2026-04-02 17:14:53 +0100

    remove build artifacts

--- .open-next/cloudflare/next-env.mjs
+++
@@ -1,3 +0,0 @@
-export const production = {"NEXT_PUBLIC_TMDB_API_KEY":"36a73ea30c5f6eab5cbcd74945ccec65","NEXT_PUBLIC_APP_URL":"http://localhost:3000"};
-export const development = {"NEXT_PUBLIC_TMDB_API_KEY":"36a73ea30c5f6eab5cbcd74945ccec65","NEXT_PUBLIC_APP_URL":"http://localhost:3000"};
-export const test = {};
